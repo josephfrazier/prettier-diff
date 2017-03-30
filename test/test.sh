9e809c75e129a7a8ed23eecb0ca12ccc1884d85f
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-30 01:00:31 -0400

    Ignore all space and blank lines in git diff
    This makes the diff more semantic, and avoids having the test script
    remove blank lines manually.

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sed '/^$/d' index.js > test/index.js.noblanks
-./node_modules/.bin/uglifyjs --comments all test/index.js.noblanks > test/index.js.uglified
-./bin/prettier-diff test/index.js.noblanks test/index.js.uglified
-rm test/index.js.noblanks test/index.js.uglified
+./node_modules/.bin/uglifyjs --comments all index.js > test/index.js.uglified
+./bin/prettier-diff index.js test/index.js.uglified
+rm test/index.js.uglified
