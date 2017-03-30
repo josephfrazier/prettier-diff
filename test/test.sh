45398950ad2ad8ceee231fe970e0f39dd55458e8
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-30 14:37:48 -0400

    Set $PATH in test script

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./node_modules/.bin/uglifyjs --comments all index.js > test/index.js.uglified
-./bin/prettier-diff index.js test/index.js.uglified
+export PATH=./bin:./node_modules/.bin:$PATH
+
+uglifyjs --comments all index.js > test/index.js.uglified
+prettier-diff index.js test/index.js.uglified
 rm test/index.js.uglified
