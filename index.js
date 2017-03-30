ed1ee7a4659ec93e1edc5bc6fdbd436784474642
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-29 21:41:14 -0400

    Run `format` script
        yarn format
    
    Note that prettier-diff shows no diff here, as expected.
    To see this, check out this commit and run:
    
        git difftool --extcmd=./bin/prettier-diff head^

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const prettier = require('prettier')
-const diff = require('diff')
+const fs = require('fs');
+const prettier = require('prettier');
+const diff = require('diff');
 
 // https://github.com/prettier/prettier/tree/a707dda53b13a6956a825609f30baead7ef08a59#api
 const defaultPrettierOptions = {
@@ -11,20 +11,33 @@ const defaultPrettierOptions = {
   singleQuote: true,
   trailingComma: 'all',
   bracketSpacing: true,
-}
+};
 
-module.exports = function ({ fromPath, toPath, fromContent, toContent, prettierOptions = defaultPrettierOptions}) {
+module.exports = function(
+  {
+    fromPath,
+    toPath,
+    fromContent,
+    toContent,
+    prettierOptions = defaultPrettierOptions,
+  }
+) {
   let fromPretty, toPretty;
 
   try {
-    fromPretty = prettier.format(fromContent, prettierOptions)
-    toPretty = prettier.format(toContent, prettierOptions)
+    fromPretty = prettier.format(fromContent, prettierOptions);
+    toPretty = prettier.format(toContent, prettierOptions);
   } catch (err) {
-    fromPretty = fromContent
-    toPretty = toContent
+    fromPretty = fromContent;
+    toPretty = toContent;
   }
 
-  const patch = diff.createTwoFilesPatch(fromPath, toPath, fromPretty, toPretty);
+  const patch = diff.createTwoFilesPatch(
+    fromPath,
+    toPath,
+    fromPretty,
+    toPretty
+  );
 
-  return {fromPretty, toPretty, patch};
-}
+  return { fromPretty, toPretty, patch };
+};
