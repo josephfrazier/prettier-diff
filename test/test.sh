3055a2c35f1470d26e738b574cd7d622e0b8c427
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-30 16:35:33 -0400

    test: check that diff is actually empty

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,5 @@ export PATH=./bin:./node_modules/.bin:$PATH
 
 sed '/^$/d' index.js > test/index.js.noblanks
 uglifyjs --comments all test/index.js.noblanks > test/index.js.uglified
-prettier-diff test/index.js.noblanks test/index.js.uglified
+prettier-diff test/index.js.noblanks test/index.js.uglified | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
 rm test/index.js.noblanks test/index.js.uglified
