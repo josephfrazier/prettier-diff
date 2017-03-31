e8d95385eb20d07d0e79d836a7fa413233dad8f3
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-31 19:03:21 -0400

    Add test fixtures to repo, don't generate on-the-fly

---
+++ test/js.js
@@ -0,0 +1,41 @@
+const prettier = require('prettier');
+const stringify = require('json-stable-stringify');
+const diff = require('diff');
+// https://github.com/prettier/prettier/tree/a707dda53b13a6956a825609f30baead7ef08a59#api
+const defaultPrettierOptions = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+};
+module.exports = function(
+  {
+    fromPath,
+    toPath,
+    fromContent,
+    toContent,
+    prettierOptions = defaultPrettierOptions,
+  }
+) {
+  let fromPretty = fromContent;
+  let toPretty = toContent;
+  // try to format JS files
+  try {
+    fromPretty = prettier.format(fromContent, prettierOptions);
+    toPretty = prettier.format(toContent, prettierOptions);
+  } catch (err) {}
+  // try to format JSON files
+  // prettier doesn't do this currently: https://github.com/prettier/prettier/issues/322
+  try {
+    fromPretty = stringify(JSON.parse(fromContent), { space: 2 });
+    toPretty = stringify(JSON.parse(toContent), { space: 2 });
+  } catch (err) {}
+  const patch = diff.createTwoFilesPatch(
+    fromPath,
+    toPath,
+    fromPretty,
+    toPretty
+  );
+  return { fromPretty: fromPretty, toPretty: toPretty, patch: patch };
+};
--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export PATH=./bin:./node_modules/.bin:$PATH
-
-sed '/^$/d' index.js > test/index.js.noblanks
-uglifyjs --comments all test/index.js.noblanks > test/index.js.uglified
-prettier-diff test/index.js.noblanks test/index.js.uglified | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
-rm test/index.js.noblanks test/index.js.uglified
+./bin/prettier-diff test/js.js test/js.js.uglified | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
