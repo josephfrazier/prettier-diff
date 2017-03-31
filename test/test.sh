d1ba82e12b94377645ce6511aab22bbf5baca044
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-31 19:11:51 -0400

    test: extract assertEmptyPrettierDiff() helper

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./bin/prettier-diff test/js.js test/js.js.uglified | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
-./bin/prettier-diff test/json.json test/json.json.uglified | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
+function assertEmptyPrettierDiff () {
+  ./bin/prettier-diff $1 $2 | tee /dev/stderr | wc -l | ag ' 0' >/dev/null
+}
+
+assertEmptyPrettierDiff test/js.js test/js.js.uglified
+assertEmptyPrettierDiff test/json.json test/json.json.uglified
