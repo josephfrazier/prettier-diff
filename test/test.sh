7706b52f91a4c898aa49a781aa1ca3a01eaab93a
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-03 04:23:30 -0400

    Handle --help and --version arguments outside difftool

--- test/test.sh
+++ test/test.sh
@@ -9,6 +9,7 @@ function containsOnce () {
   cat | grep "$1" | wc -l | grep ' 1$' >/dev/null
 }
 
+./prettier-diff --version | containsOnce "$(grep version package.json | cut -d '"' -f 4)"
 assertEmptyPrettierDiff test/js.js test/js.uglified.js
 assertEmptyPrettierDiff test/json.json test/json.uglified.json
 ./prettier-diff test/1.json test/2.json | containsOnce key1
