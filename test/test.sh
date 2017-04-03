7a5b7a4d3b79ea9a2aff33965134ee3af523ff24
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-03 03:57:17 -0400

    Move bin/prettier-diff to prettier-diff

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 function assertEmptyPrettierDiff () {
-  ./bin/prettier-diff $1 $2 | tee /dev/stderr | wc -l | grep ' 0' >/dev/null
+  ./prettier-diff $1 $2 | tee /dev/stderr | wc -l | grep ' 0' >/dev/null
 }
 
 function containsOnce () {
@@ -11,8 +11,8 @@ function containsOnce () {
 
 assertEmptyPrettierDiff test/js.js test/js.uglified.js
 assertEmptyPrettierDiff test/json.json test/json.uglified.json
-./bin/prettier-diff test/1.json test/2.json | containsOnce key1
-./bin/prettier-diff test/1.json test/2.json | containsOnce '^    "key1"'
-./bin/prettier-diff test/1.js test/2.js | containsOnce '^        key1'
-./bin/prettier-diff test/1.js test/2.js | grep '        ],' | wc -l | grep ' 2$' >/dev/null
-./bin/prettier-diff f763ab1dd2bf1b9f05865254aa843cffb061d943^ f763ab1dd2bf1b9f05865254aa843cffb061d943 | containsOnce 'added: .eslintrc.js'
+./prettier-diff test/1.json test/2.json | containsOnce key1
+./prettier-diff test/1.json test/2.json | containsOnce '^    "key1"'
+./prettier-diff test/1.js test/2.js | containsOnce '^        key1'
+./prettier-diff test/1.js test/2.js | grep '        ],' | wc -l | grep ' 2$' >/dev/null
+./prettier-diff f763ab1dd2bf1b9f05865254aa843cffb061d943^ f763ab1dd2bf1b9f05865254aa843cffb061d943 | containsOnce 'added: .eslintrc.js'
