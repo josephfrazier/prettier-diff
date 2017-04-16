aaec39a5ff4082b9e5a6d551c3a7bc65aee2e4e0
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-16 00:38:59 -0400

    Only try to format JSON if prettier fails

--- bin/prettier-diff.js
+++ bin/prettier-diff.js
@@ -66,21 +66,21 @@ if (process.env.GIT_PREFIX === undefined) {
   // try to format JS files
   try {
     pretty = prettier.format(content, prettierOptions)
-  } catch (err) {}
-
-  // try to format JSON files
-  // prettier doesn't do this currently: https://github.com/prettier/prettier/issues/322
-  try {
-    const sorted = stringifyAligned(
-      JSON.parse(stringifySorted(JSON.parse(content))),
-      null,
-      prettierOptions.tabWidth,
-      true
-    )
-    // Put a comma after strings, numbers, objects, arrays, `true`, `false`, or
-    // `null` at the end of a line. See the grammar at http://json.org/
-    pretty = sorted.replace(/(.["\d}\]el])$/gm, '$1,')
-  } catch (err) {}
+  } catch (err) {
+    // try to format JSON files
+    // prettier doesn't do this currently: https://github.com/prettier/prettier/issues/322
+    try {
+      const sorted = stringifyAligned(
+        JSON.parse(stringifySorted(JSON.parse(content))),
+        null,
+        prettierOptions.tabWidth,
+        true
+      )
+      // Put a comma after strings, numbers, objects, arrays, `true`, `false`, or
+      // `null` at the end of a line. See the grammar at http://json.org/
+      pretty = sorted.replace(/(.["\d}\]el])$/gm, '$1,')
+    } catch (err) {}
+  }
 
   console.log(pretty)
 }
